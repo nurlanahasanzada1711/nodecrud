a7faa917ab9856c208a6c14aa23837898fbbf935
Elnaraa9 <7khtht3@code.edu.az>
2023-05-17 09:53:54 +0400

    helloo

--- server/index.js
+++ server/index.js
@@ -1,42 +1,186 @@
 const express = require("express");
-const crypto = require("crypto");
-
 const app = express();
+const crypto = require("crypto");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const PORT = 8080;
 
-//data
-const ARTISTS = [
-    {
-      id: 1,
-      name: "Tyler the Creator",
-      imageURL:
-        "https://i.scdn.co/image/ab676161000051748278b782cbb5a3963db88ada",
-      age: 70,
-    },
-    {
-      id: 2,
-      name: "Kanye West",
-      imageURL:
-        "https://www.thenews.com.pk/assets/uploads/updates/2023-05-15/1070367_8385471_Untitled-1_updates.jpg",
-      age: 45,
-    },
-  ];
-  
+app.use(cors());
 
-app.get('/api', (req, res) => {
-    res.send('Welcome to OUR API')
-})
+
+//data
+const STUDENTS = [
+  {
+    id: 1,
+    name: "Nurlana",
+    surname: "Hasanzada",
+    birthdate: "17.11.2001",
+    faculty:"Computer Science",
+    occupation: "Front-end developer",
+    isMarriend: false,
+    GPA: 80
+  },
+  {
+    id: 2,
+    name: "Laman",
+    surname: "Poladli",
+    birthdate: "04.11.2002",
+    faculty:"Computer Science",
+    occupation: "Interior design",
+    isMarriend: false,
+    GPA: 90
+  },
+  {
+    id: 3,
+    name: "Aysel",
+    surname: "Amrahovba",
+    birthdate: "06.01.2002",
+    faculty:"International relationship",
+    occupation: "Back-end developer",
+    isMarriend: false,
+    GPA: 85
+  },
+  {
+    id: 4,
+    name: "Ayshan",
+    surname: "Azizova",
+    birthdate: "31.12.2001",
+    faculty:"Computer Science",
+    occupation: "Game developer",
+    isMarriend: false,
+    GPA: 78
+  },
+  {
+    id: 5,
+    name: "Baba",
+    surname: "Kazimov",
+    birthdate: "25.02.2002",
+    faculty:"Computer Science",
+    occupation: "Front-end developer",
+    isMarriend: false,
+    GPA: 83
+  },
+  {
+    id: 6,
+    name: "Cavidan",
+    surname: "Shushayev",
+    birthdate: "12.05.2002",
+    faculty:"Computer Science",
+    occupation: "Front-end developer",
+    isMarriend: false,
+    GPA: 88
+  }
+];
+
+
+
+app.get("/api", (req, res) => {
+  res.send("Welcome to Our API!");
+});
 
 //CRUD - CREATE READ UPDATE DELETE
 
 //get All Artists
-app.get('/api/artists', (req,res) => {
+app.get("/api/students", (req, res) => {
+  const { name } = req.query;
+  if (name === undefined) {
     res.status(200).send({
-        data: ARTISTS,
-        message:'Data get success!'
-    })
-})
+      data: STUDENTS,
+      message: "data get success!",
+    });
+  } else {
+    res.status(200).send({
+      data: STUDENTS.filter(
+        (x) => x.name.toLowerCase().trim().includes(name.toLowerCase().trim())
+      ),
+      message: "data get success!",
+    });
+  }
+});
+
+//get Artists by ID
+app.get("/api/students/:id", (req, res) => {
+  const id = req.params.id;
+  const students = STUDENTS.find((x) => x.id === parseInt(id));
+  if (!students) {
+    console.log("test");
+    res.status(204).send("Student not found!");
+  } else {
+    res.status(200).send({
+      data: students,
+      message: "data get success!",
+    });
+  }
+});
+
+
+//delete artist by ID
+app.delete("/api/students/:id", (req, res) => {
+  const id = req.params.id;
+  const students = STUDENTS.find((x) => x.id == id);
+  if (  students === undefined) {
+    res.status(404).send("Student not found");
+  } else {
+    const idx = STUDENTS.indexOf(students);
+    STUDENTS.splice(idx, 1);
+    res.status(203).send({
+      data: students,
+      message: "Student deleted successfully",
+    });
+  }
+});
+
+//post
+app.post("/api/students",(req, res) => {
+  const { name, surname, birthdate, occupation,isMarriend, GPA } = req.body;
+  const newStudent = {
+    id: crypto.randomUUID(),
+    name: name,
+    surname:surname,
+    birthdate:birthdate,
+    occupation:occupation,
+    isMarriend:isMarriend,
+    GPA:GPA,
+  };
+  STUDENTS.push(newStudent);
+  res.status(201).send("created");
+});
+
+
+//put
+app.put("/api/students/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, surname, birthdate, occupation,isMarriend, GPA  } = req.body;
+  const existedStudent = STUDENTS.find((x) => x.id == id);
+  if (existedStudent == undefined) {
+    res.status(404).send("Student not found!");
+  } else {
+    if (name) {
+      existedStudent.name = name;
+    }
+    if (surname) {
+      existedStudent.surname = surname;
+    }
+    if (birthdate) {
+        existedStudent.birthdate = birthdate;
+      }
+      if (occupation) {
+        existedStudent.occupation = occupation;
+      }
+      if (isMarriend) {
+        existedStudent.isMarriend = isMarriend;
+      }
+      if (GPA) {
+        existedStudent.GPA = GPA;
+      }
+    
+    res.status(200).send(`student: ${existedStudent.name}`);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Node App listening on port ${PORT}`)
-})
+    console.log(`NODE APP listening on port ${PORT}`);
+});
